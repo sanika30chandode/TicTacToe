a8cec20e2283c8a9e96b5c927410c4bbd92ebd7f
Sanika Chandode <sanu30chandode@gmail.com>
2022-09-17 14:09:46 +0530

    app updated

--- tic_tac_toe/lib/main.dart
+++ tic_tac_toe/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/ui/theme/color.dart';
+import 'package:tic_tac_toe/utils/gamelogic.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,57 +13,152 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: GameScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GameScreen> createState() => _GameScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GameScreenState extends State<GameScreen> {
+  //adding the necessary variables
+  String lastValue = "X";
+  bool gameOver = false;
+  int turn = 0; // the number of turn
+  String result = "";
+  List<int> scoreboard = [0, 0, 0, 0, 0, 0, 0, 0];
+  // the score are for the different combination of the game[row 1,2,3, Col 1, 2,3, Diagonal 1, 2]
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  //let's declare a new Game components
+  Game game = Game();
+
+  //let's initiate the GameBoard
+  @override
+  void initState() {
+    //TODO: implement initState
+    super.initState();
+    game.board = Game.initGameBoard();
+    print(game.board);
   }
 
   @override
   Widget build(BuildContext context) {
+    double boardWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+        //the first step is organise our project folder structure
+        backgroundColor: MainColor.primaryColor,
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "It's $lastValue Turn".toUpperCase(),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 58,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            // now we will make the game board
+            // but first we will create a Game class that will contain all the data and method that we will be going to need
+            Container(
+              width: boardWidth,
+              height: boardWidth,
+              child: GridView.count(
+                crossAxisCount: Game.boardlength ~/
+                    3, // the ~/ operator alllows you to integer and return and int as a result
+                padding: EdgeInsets.all(16.0),
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+                children: List.generate(Game.boardlength, (index) {
+                  return InkWell(
+                    onTap: gameOver
+                        ? null
+                        : () {
+                            //when we click we need to add new value to the board and refresh the screen
+                            //1
+                            //2
+                            //3
+                            //3 now let's create a button to repeat the game
+                            // 2 now we need to apply the the click only if the field is empty
+
+                            //1 we also need to toggle the player
+
+                            if (game.board![index] == "") {
+                              setState(() {
+                                game.board![index] = lastValue;
+                                turn++;
+                                gameOver = game.winnerCheck(
+                                    lastValue, index, scoreboard, 3);
+                                if (gameOver) {
+                                  result = "$lastValue is the Winner";
+                                } else if (!gameOver && turn == 9) {
+                                  result = "It's a Draw!";
+                                  gameOver = true;
+                                }
+                                if (lastValue == "X")
+                                  lastValue = "0";
+                                else
+                                  lastValue = "X";
+                              });
+                            }
+                          },
+                    child: Container(
+                      width: Game.blockSize,
+                      height: Game.blockSize,
+                      decoration: BoxDecoration(
+                        color: MainColor.secondaryColor,
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          game.board![index],
+                          style: TextStyle(
+                            color: game.board![index] == "X"
+                                ? Colors.blue
+                                : Colors.pink,
+                            fontSize: 64.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              result,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 54.0,
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                setState(() {
+                  //erase the board
+                  game.board = Game.initGameBoard();
+                  lastValue = "X";
+                  gameOver = false;
+                  turn = 0;
+                  result = "";
+                  scoreboard = [0, 0, 0, 0, 0, 0, 0, 0];
+                });
+              },
+              icon: Icon(Icons.replay),
+              label: Text("Repeat the Game"),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        ));
   }
 }
---
+++ tic_tac_toe/lib/utils/gamelogic.dart
@@ -0,0 +1,39 @@
+class Player {
+  static const x = "X";
+  static const o = "O";
+  static const empty = "";
+}
+
+class Game {
+  static final boardlength = 9; //we will create a board of 3*3 blocks
+  static final blockSize = 100.0;
+
+  //Creating the empty board
+  List<String>? board;
+
+  static List<String>? initGameBoard() =>
+      List.generate(boardlength, (index) => Player.empty);
+
+  // now we need to check the build the winner check algorithm
+  // for this we need first to declare a scoreboard in our main file
+
+  bool winnerCheck(
+      String player, int index, List<int> scoreboard, int gridSize) {
+    //first let's declare the row and col
+    int row = index ~/ 3;
+    int col = index % 3;
+    int score = player == "X" ? 1 : -1;
+
+    scoreboard[row] += score;
+    scoreboard[gridSize + col] += score;
+    if (row == col) scoreboard[2 * gridSize] += score;
+    if (gridSize - 1 - col == row) scoreboard[2 * gridSize + 1] += score;
+
+    //checking if we have 3 or -3 in the score board
+    if (scoreboard.contains(3) || scoreboard.contains(-3)) {
+      return true;
+    }
+    ;
+    return false;
+  }
+}
